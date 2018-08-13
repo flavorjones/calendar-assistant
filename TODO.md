6c8b4d752aa3e64044c3d5b6764a1dab8b25242b
Mike Dalessio <mike.dalessio@gmail.com>
2018-08-13 09:51:23 -0400

    add .calendar_list_for
    and done with experiments

--- TODO.md
+++ TODO.md
@@ -3,6 +3,14 @@
 ## Experiments
 
 - [x] how do I authenticate without writing creds to disk
-- can I determine people's native timezones
-- can I resolve email address to a person's name
-- can I see other people's busy/free status
+- [x] can I determine people's native timezones
+- [-] can I resolve email address to a person's name
+- [x] can I see other people's busy/free status
+
+
+## Notes
+
+If an event is only visible via free/busy status, it will be `visibility: private`
+
+It looks like events we can see are only those that are `status: confirmed`
+
--- calendar-assistant.rb
+++ calendar-assistant.rb
@@ -18,17 +18,22 @@ module CalendarAssistant
     File.open(CALENDAR_TOKENS_FILE, "w") { |f| f.write calendar_tokens.to_yaml }
   end
 
-  def self.calendar_for calendar_id, refresh_token=nil
+  def self.params_for calendar_id
     client_id = JSON.parse(File.read(CLIENT_ID_FILE))
-
-    params = {
+    {
       :client_id     => client_id["installed"]["client_id"],
       :client_secret => client_id["installed"]["client_secret"],
       :calendar      => CALENDAR_ID,
       :redirect_url  => "urn:ietf:wg:oauth:2.0:oob",
       :refresh_token => CalendarAssistant.token_for(CALENDAR_ID),
     }
+  end
 
-    Google::Calendar.new params
+  def self.calendar_for calendar_id
+    Google::Calendar.new params_for(calendar_id)
+  end
+
+  def self.calendar_list_for calendar_id
+    Google::CalendarList.new params_for(calendar_id)
   end
 end
