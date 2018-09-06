a2f3371e7492eefbf95fb1189387925657d824ae
Mike Dalessio <mike.dalessio@gmail.com>
2018-09-06 12:28:26 -0400

    remove old class extensions

--- lib/calendar_assistant/event_extensions.rb
+++ lib/calendar_assistant/event_extensions.rb
@@ -65,34 +65,3 @@ class Google::Apis::CalendarV3::EventDateTime
     @date_time.strftime "%Y-%m-%d %H:%M"
   end
 end
-
-module Google
-  class Event
-    def to_assistant_s
-      if assistant_location_event?
-        if Event.parse_time(end_time) - Event.parse_time(start_time) <= 1.day
-          sprintf "%-23.23s |                         | %-40.40s",
-                  Event.assistant_date(Event.parse_time(start_time)), title
-        else
-          sprintf "%-23.23s | %-23.23s | %-40.40s",
-                  Event.assistant_date(Event.parse_time(start_time)),
-                  Event.assistant_date(Event.parse_time(end_time) - 1.day), title
-        end
-      else
-        sprintf "%23.23s | %23.23s | %-40.40s",
-                Event.assistant_time(Event.parse_time(start_time)),
-                Event.assistant_time(Event.parse_time(end_time)), title
-      end
-    end
-
-    private
-
-    def self.assistant_time t
-      t.getlocal.strftime("%Y-%m-%d %H:%M:%S %Z")      
-    end
-
-    def self.assistant_date t
-      t.getlocal.strftime("%Y-%m-%d %a")
-    end
-  end
-end
