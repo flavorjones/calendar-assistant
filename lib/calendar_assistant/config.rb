3725f082dc920181a3d2dbab746dd8491b05a74f
Mike Dalessio <mike.dalessio@gmail.com>
2018-10-14 09:48:39 -0400

    handle failure-to-parse config file
    and allow an IO to be passed in to make testing easier
    
    related to #22

--- lib/calendar_assistant/config.rb
+++ lib/calendar_assistant/config.rb
@@ -2,13 +2,25 @@ require "toml"
 
 class CalendarAssistant
   class Config
+    class TomlParseFailure < CalendarAssistant::BaseException ; end
+
     CONFIG_FILE_PATH = File.join ENV["HOME"], ".calendar-assistant"
 
     attr_reader :user_config
 
-    def initialize config_file_path: CONFIG_FILE_PATH
-      @user_config = if File.exist? config_file_path
-                       TOML.load_file config_file_path
+    def initialize config_file_path: CONFIG_FILE_PATH, config_io: nil
+      @user_config = if config_io
+                       begin
+                         TOML.load config_io.read
+                       rescue Exception => e
+                         raise TomlParseFailure, "could not parse IO stream: #{e}"
+                       end
+                     elsif File.exist? config_file_path
+                       begin
+                         TOML.load_file config_file_path
+                       rescue Exception => e
+                         raise TomlParseFailure, "could not parse #{config_file_path}: #{e}"
+                       end
                      else
                        Hash.new
                      end
--- spec/calendar_assistant/config_spec.rb
+++ spec/calendar_assistant/config_spec.rb
@@ -1,16 +1,39 @@
 describe CalendarAssistant::Config do
   describe ".new" do
-    context "config file exists" do
-      it "reads the TOML and makes it available as #user_config" do
-        expect(described_class.new(config_file_path: artifact_path("user_config_1")).user_config).
-          to eq({"settings" => {"start-of-day" => "8am", "end-of-day" => "5:30pm"}})
+    context "passed a config file" do
+      context "config file exists" do
+        it "reads the TOML and makes it available as #user_config" do
+          expect(described_class.new(config_file_path: artifact_path("user_config_1")).user_config).
+            to eq({"settings" => {"start-of-day" => "8am", "end-of-day" => "5:30pm"}})
+        end
+
+        context "and is not TOML" do
+          it "raises an exception" do
+            expect { described_class.new(config_file_path: artifact_path("user_config_bad")) }.
+              to raise_exception(CalendarAssistant::BaseException)
+          end
+        end
+      end
+
+      context "config file does not exist" do
+        it "initializes #user_config to an empty hash" do
+          expect(described_class.new(config_file_path: "/path/to/nonexistent/file").user_config).
+            to eq({})
+        end
       end
     end
 
-    context "config file does not exist" do
-      it "initializes #user_config to an empty hash" do
-        expect(described_class.new(config_file_path: "/path/to/nonexistent/file").user_config).
-          to eq({})
+    context "passed an IO object" do
+      it "reads the TOML and makes it available as #user_config" do
+        expect(described_class.new(config_io: StringIO.new("foo = 123")).user_config).
+          to eq("foo" => 123)
+      end
+
+      context "and is not TOML" do
+        it "raises an exception" do
+          expect { described_class.new(config_io: StringIO.new("foo: 123")) }.
+            to raise_exception(CalendarAssistant::BaseException)
+        end
       end
     end
   end
