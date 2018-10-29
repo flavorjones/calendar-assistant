6e1b540fbe999c0d3fcee55de3e3c1eb342b8ae7
Mik Freedman <pair+mik@pivotal.io>
2018-10-29 13:46:03 -0400

    Finding a URI fails gracefully for string helpers
    Allow for nil parameter
    
    Fixes #32

--- lib/calendar_assistant/string_helpers.rb
+++ lib/calendar_assistant/string_helpers.rb
@@ -3,6 +3,7 @@ require "uri"
 class CalendarAssistant
   module StringHelpers
     def self.find_uri_for_domain string, domain
+      return unless string
       URI::Parser.new.extract(string).each do |uri_string|
         uri = URI.parse uri_string
         return uri_string if uri.hostname =~ /\.#{domain}$/
