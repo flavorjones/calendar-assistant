2f4a11d623e5a2786e46f4f9e4307bcd96923dc7
Mik Freedman <github@michael-freedman.com>
2018-11-27 08:57:43 -0500

    do not require faker in EventFactory

--- spec/helpers/event_factory.rb
+++ spec/helpers/event_factory.rb
@@ -1,6 +1,4 @@
-require 'faker'
 require 'securerandom'
-
 class EventFactory
   def initialize(service:, calendar_id: "primary")
     @event_repository = CalendarAssistant::EventRepository.new(service, calendar_id)
