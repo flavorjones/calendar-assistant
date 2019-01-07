2952a6759797e1568efc50e4c60f890751476686
Mike Dalessio <mike.dalessio@gmail.com>
2019-01-07 00:05:01 -0500

    add Code Climate test coverage reporting
    to both master and PR jobs
    
    Closes #30

--- concourse/tasks/rake-spec/run.sh
+++ concourse/tasks/rake-spec/run.sh
@@ -2,9 +2,15 @@
 
 set -e -x -u
 
+source "$(dirname "$0")/../../shared/code-climate.sh"
+
 pushd calendar-assistant
+
+  code-climate-setup
 
   bundle install --without=optional
   bundle exec rake
+
+  code-climate-shipit
 
 popd
