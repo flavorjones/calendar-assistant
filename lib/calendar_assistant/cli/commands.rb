c57b2791fc16e2f2e9318561222b8e5d0a91a587
Mik Freedman <pair+mfreedman@pivotal.io>
2019-01-20 20:02:04 -0500

    extract command service class

--- lib/calendar_assistant/cli/commands.rb
+++ lib/calendar_assistant/cli/commands.rb
@@ -1,5 +1,3 @@
-require "calendar_assistant/cli/helpers"
-
 class CalendarAssistant
   module CLI
     class Commands < Thor
@@ -54,7 +52,7 @@ class CalendarAssistant
 
       def version
         return if handle_help_args
-        out.puts CalendarAssistant::VERSION
+        command_service.out.puts CalendarAssistant::VERSION
       end
 
 
@@ -64,7 +62,7 @@ class CalendarAssistant
       def config
         return if handle_help_args
         settings = CalendarAssistant::CLI::Config.new.settings
-        out.puts TOML::Generator.new({CalendarAssistant::Config::Keys::SETTINGS => settings}).body
+        command_service.out.puts TOML::Generator.new({CalendarAssistant::Config::Keys::SETTINGS => settings}).body
       end
 
 
@@ -84,14 +82,14 @@ class CalendarAssistant
         # TODO ugh see #34 for advice on how to clean this up
         return if handle_help_args
         if File.exist? CalendarAssistant::CLI::Authorizer::CREDENTIALS_PATH
-          out.puts sprintf("Credentials already exist in %s",
-                           CalendarAssistant::CLI::Authorizer::CREDENTIALS_PATH)
+          command_service.out.puts sprintf("Credentials already exist in %s",
+                                           CalendarAssistant::CLI::Authorizer::CREDENTIALS_PATH)
           return
         end
 
-        out.launch "https://developers.google.com/calendar/quickstart/ruby"
+        command_service.out.launch "https://developers.google.com/calendar/quickstart/ruby"
         sleep 1
-        out.puts <<~EOT
+        command_service.out.puts <<~EOT
         Please click on "ENABLE THE GOOGLE CALENDAR API" and either create a new project or select an existing project.
 
         (If you create a new project, name it something like "yourname-calendar-assistant" so you remember why it exists.)
@@ -101,13 +99,13 @@ class CalendarAssistant
         Finally, paste the contents of the downloaded file here (it should be a complete JSON object):
         EOT
 
-        json = out.prompt "Paste JSON here"
+        json = command_service.out.prompt "Paste JSON here"
         File.open(CalendarAssistant::CLI::Authorizer::CREDENTIALS_PATH, "w") do |f|
           f.write json
         end
         FileUtils.chmod 0600, CalendarAssistant::CLI::Authorizer::CREDENTIALS_PATH
 
-        out.puts "\nOK! Your next step is to run `calendar-assistant authorize`."
+        command_service.out.puts "\nOK! Your next step is to run `calendar-assistant authorize`."
       end
 
 
@@ -129,7 +127,7 @@ class CalendarAssistant
         return if handle_help_args
         return help! if profile_name.nil?
 
-        get_authorizer(profile_name: profile_name).authorize
+        command_service.authorizer(profile_name: profile_name).authorize
 
         puts "\nYou're authorized!\n\n"
       end
@@ -142,7 +140,7 @@ class CalendarAssistant
       with_predicate_options
       long_desc predicate_option_descriptions
       def lint datespec = "today"
-        calendar_assistant(datespec) do |ca, date|
+        calendar_assistant(datespec) do |ca, date, out|
           event_set = ca.lint_events date
           out.print_events ca, event_set, presenter_class: CalendarAssistant::CLI::LinterEventSetPresenter
         end
@@ -160,7 +158,7 @@ class CalendarAssistant
       with_predicate_options
       long_desc predicate_option_descriptions
       def show datespec = "today"
-        calendar_assistant(datespec) do |ca, date|
+        calendar_assistant(datespec) do |ca, date, out|
           event_set = ca.find_events date
           out.print_events ca, event_set
         end
@@ -179,15 +177,15 @@ class CalendarAssistant
       def join timespec = "now"
         return if handle_help_args
         set_formatting
-        ca = CalendarAssistant.new get_config, service: service
+        ca = CalendarAssistant.new command_service.config, service: command_service.service
         ca.in_env do
           event_set, url = CalendarAssistant::CLI::Helpers.find_av_uri ca, timespec
           if !event_set.empty?
-            out.print_events ca, event_set
-            out.puts url
-            out.launch url if options[CalendarAssistant::Config::Keys::Options::JOIN]
+            command_service.out.print_events ca, event_set
+            command_service.out.puts url
+            command_service.out.launch url if options[CalendarAssistant::Config::Keys::Options::JOIN]
           else
-            out.puts "Could not find a meeting '#{timespec}' with a video call to join."
+            command_service.out.puts "Could not find a meeting '#{timespec}' with a video call to join."
           end
         end
       end
@@ -200,7 +198,7 @@ class CalendarAssistant
       with_predicate_options
       long_desc predicate_option_descriptions
       def location datespec = "today"
-        calendar_assistant(datespec) do |ca, date|
+        calendar_assistant(datespec) do |ca, date, out|
           event_set = ca.find_location_events date
           out.print_events ca, event_set
         end
@@ -216,7 +214,7 @@ class CalendarAssistant
       def location_set location = nil, datespec = "today"
         return help! if location.nil?
 
-        calendar_assistant(datespec) do |ca, date|
+        calendar_assistant(datespec) do |ca, date, out|
           event_set = ca.create_location_event date, location
           out.print_events ca, event_set
         end
@@ -249,7 +247,7 @@ class CalendarAssistant
       with_predicate_options
       long_desc predicate_option_descriptions
       def availability datespec = "today"
-        calendar_assistant(datespec) do |ca, date|
+        calendar_assistant(datespec) do |ca, date, out|
           event_set = ca.availability date
           out.print_available_blocks ca, event_set
         end
@@ -261,36 +259,14 @@ class CalendarAssistant
         Rainbow.enabled = !!options[:formatting]
       end
 
-      def service
-        @service ||= begin
-          if filename = get_config.setting(Config::Keys::Options::LOCAL_STORE)
-            CalendarAssistant::LocalService.new(file: filename)
-          else
-            get_authorizer.service
-          end
-        end
+      def command_service
+        @command_service ||= CommandService.new(options: options)
       end
 
-      def get_authorizer(profile_name: get_config.profile_name, token_store: get_config.token_store)
-        @authorizer ||= {}
-        @authorizer[profile_name] ||= Authorizer.new(profile_name, token_store)
-      end
-
-      def calendar_assistant datespec = "today"
+      def calendar_assistant datespec = "today", &block
         return if handle_help_args
         set_formatting
-        ca = CalendarAssistant.new(get_config, service: service)
-        ca.in_env do
-          yield(ca, CalendarAssistant::CLI::Helpers.parse_datespec(datespec))
-        end
-      end
-
-      def get_config
-        @config ||= CalendarAssistant::CLI::Config.new(options: options)
-      end
-
-      def out
-        @out ||= CalendarAssistant::CLI::Printer.new
+        command_service.calendar_assistant(datespec, &block)
       end
 
       def help!
