2d1ab525dc192fa6832a505d4eb0b3b1982ef85e
Mik Freedman <github@michael-freedman.com>
2019-02-02 22:35:27 -0500

    raise a specific error when location config is not valid
    [Closes #95]

--- lib/calendar_assistant/location_config_validator.rb
+++ lib/calendar_assistant/location_config_validator.rb
@@ -1,12 +1,15 @@
 # coding: utf-8
 class CalendarAssistant
   class LocationConfigValidator
+    class LocationConfigValidationException < CalendarAssistant::BaseException;
+    end
+
     def self.valid?(config)
       return if (config.calendar_ids - [ Config::DEFAULT_CALENDAR_ID ]).empty?
       return if !!config[CalendarAssistant::Config::Keys::Settings::NICKNAME]
       return if !!config[CalendarAssistant::Config::Keys::Options::FORCE]
 
-      raise BaseException, "Managing location across multiple calendars when a nickname is not set is not recommended, use --force to override"
+      raise LocationConfigValidationException, "Managing location across multiple calendars when a nickname is not set is not recommended, use --force to override"
     end
   end
 end
