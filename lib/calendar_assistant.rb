a51a934ef91079e3f9a9aad33b5ea9f9c49bf4a8
Mike Dalessio <mike.dalessio@gmail.com>
2024-07-27 14:36:18 -0400

    dump autoloading and bump activesupport
    the autoloading wasn't working right anymore, and is a complexity that
    is probably not worth the perf benefit anyway

--- lib/calendar_assistant.rb
+++ lib/calendar_assistant.rb
@@ -1,23 +1,23 @@
 #
 #  stdlib
 #
-autoload :YAML, "yaml"
-autoload :URI, "uri"
-autoload :Set, "set"
-autoload :FileUtils, "fileutils"
+require "yaml"
+require "uri"
+require "set"
+require "fileutils"
 
 #
 #  gem dependencies
 #
-autoload :BusinessTime, "business_time"
-autoload :Chronic, "chronic"
-autoload :ChronicDuration, "chronic_duration"
-autoload :Google, "calendar_assistant/extensions/google_apis_extensions"
-autoload :Launchy, "calendar_assistant/extensions/launchy_extensions"
-autoload :TomlRB, "toml-rb"
-autoload :Thor, "thor"
-require "calendar_assistant/extensions/rainbow_extensions" # Rainbow() doesn't trigger autoload
-require "active_support/time" # Time doesn't trigger autoload
+require "business_time"
+require "chronic"
+require "chronic_duration"
+require "calendar_assistant/extensions/google_apis_extensions"
+require "calendar_assistant/extensions/launchy_extensions"
+require "toml-rb"
+require "thor"
+require "calendar_assistant/extensions/rainbow_extensions"
+require "active_support"
 
 #
 #  CalendarAssistant and associated classes
@@ -25,20 +25,20 @@ require "active_support/time" # Time doesn't trigger autoload
 require "calendar_assistant/calendar_assistant"
 
 class CalendarAssistant
-  autoload :VERSION, "calendar_assistant/version"
-  autoload :Config, "calendar_assistant/config"
-  autoload :StringHelpers, "calendar_assistant/string_helpers"
-  autoload :DateHelpers, "calendar_assistant/date_helpers"
-  autoload :HasDuration, "calendar_assistant/has_duration"
-  autoload :AvailableBlock, "calendar_assistant/available_block"
-  autoload :Event, "calendar_assistant/event"
-  autoload :EventRepository, "calendar_assistant/event_repository"
-  autoload :EventRepositoryFactory, "calendar_assistant/event_repository_factory"
-  autoload :EventSet, "calendar_assistant/event_set"
-  autoload :Scheduler, "calendar_assistant/scheduler"
-  autoload :LocalService, "calendar_assistant/local_service"
-  autoload :LocationEventRepository, "calendar_assistant/location_event_repository"
-  autoload :LintEventRepository, "calendar_assistant/lint_event_repository"
-  autoload :PredicateCollection, "calendar_assistant/predicate_collection"
-  autoload :LocationConfigValidator, "calendar_assistant/location_config_validator"
+  require "calendar_assistant/version"
+  require "calendar_assistant/config"
+  require "calendar_assistant/string_helpers"
+  require "calendar_assistant/date_helpers"
+  require "calendar_assistant/has_duration"
+  require "calendar_assistant/available_block"
+  require "calendar_assistant/event"
+  require "calendar_assistant/event_repository"
+  require "calendar_assistant/event_repository_factory"
+  require "calendar_assistant/event_set"
+  require "calendar_assistant/scheduler"
+  require "calendar_assistant/local_service"
+  require "calendar_assistant/location_event_repository"
+  require "calendar_assistant/lint_event_repository"
+  require "calendar_assistant/predicate_collection"
+  require "calendar_assistant/location_config_validator"
 end
